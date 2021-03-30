cc948bf1e18afd5cbcf4c59434704b0268a97f29
Alyssa-Margalit <alyssam0@usc.edu>
2021-03-29 23:12:15 -0700

    initial

--- rpi.py
+++ rpi.py
@@ -26,15 +26,15 @@ if __name__ == '__main__':
 		distance = grovepi.ultrasonicRead(sonic)
 		dist = int(distance)
 		r = dist
-		print("red value: "+ r)
+		print("red value: "+ str(r))
 		distance2 = grovepi.ultrasonicRead(sonic2)
 		dist2 = int(distance2)
 		b = dist2
-		print("blue value: "+ b)
+		print("blue value: "+ str(b))
 		button_status = digitalRead(button)
 		if button_status:
 			g = g+2
-			print("green value: "+ g)
+			print("green value: "+ srt(g))
 		setRGB(r,g,b)
 		time.sleep(1)
        
